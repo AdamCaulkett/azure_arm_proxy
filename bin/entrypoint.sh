d0bc0c6cf5b77c21449cd3ac512d3708789657a2
Slava Khrenov <slava@rightscale.com>
2016-05-27 00:40:25 +0300

    CF-150 - don't use localhost to listen

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -17,7 +17,7 @@ function entrypoint() {
             ;;
     esac
 
-    exec /srv/azure_arm_proxy/azure_v2/azure_v2 --listen="localhost:8083" --prefix="/azure_v2" > /dev/null
+    exec /srv/azure_arm_proxy/azure_v2/azure_v2 --listen=":8083" --prefix="/azure_v2" > /dev/null
     #exec /srv/azure_arm_proxy/azure_v2/azure_v2 --listen="localhost:8084" --prefix="/azure_v2" > /dev/null
     #exec /srv/azure_arm_proxy/azure_v2/azure_v2 --listen="localhost:8085" --prefix="/azure_v2" > /dev/null
 }
